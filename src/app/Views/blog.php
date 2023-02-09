ab181c3f7fd10083a38240d37ae5aa102ee80b89
arjuneyy <armanipes.social@gmail.com>
2023-02-09 18:27:51 +0800

    Styling sample blog items with bootstrap and view cell.

--- src/app/Views/blog.php
+++ src/app/Views/blog.php
@@ -1,9 +1,7 @@
 <h1><?= $title ?></h1>
 
-<div>
+<div class="row">
     <?php foreach($posts as $post): ?>
-        <h3> <?= $post ?></h3>
-        <img src="/assets/images/image1.png" style="width: 300px; height: auto;" alt="">
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia id aut, deleniti natus eveniet itaque iste magnam repellendus dolor quaerat similique atque at, maiores earum qui pariatur dolore odio vitae?</p>
+        <?= view_cell('\App\Libraries\Blog::postItem', ['title' => $post]) ?>
     <?php endforeach; ?>
 </div>
---
+++ src/app/Views/components/post_item.php
@@ -0,0 +1,8 @@
+<div class="card col-12 col-sm-6 col-md-4" style="width: 18rem;">
+  <img src="/assets/images/image1.png"  class="card-img-top" alt="">
+  <div class="card-body">
+    <h5 class="card-title"><?= $title ?></h5>
+    <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia id aut, deleniti natus eveniet itaque iste magnam repellendus dolor quaerat similique atque at, maiores earum qui pariatur dolore odio vitae?</p>
+    <a href="#" class="btn btn-primary">Read more</a>  
+</div>
+</div>
